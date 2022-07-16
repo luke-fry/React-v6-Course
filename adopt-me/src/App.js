23f092af959457fb7801c8d013424afd9a0122f5
Luke Fry <luke.fry096@gmail.com>
2022-07-16 23:11:27 +1000

    05 useEffect

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,32 +1,16 @@
-/* global React ReactDOM */
+import React from "react";
+import ReactDOM from "react-dom";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Jinx",
-      animal: "Dog",
-      breed: "Husky",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
